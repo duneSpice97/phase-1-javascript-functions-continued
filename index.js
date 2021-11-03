e4fd8c7939db7c02fd5007ed06f9e7c9b891e84d
duneSpice97 <89211252+duneSpice97@users.noreply.github.com>
2021-11-03 11:34:34 -0600

    commit tree bd7fc5a522df74bbeaf584eba0db7256f4f258b5

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Your code here
+saturdayFun();
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+};
+
+saturdayFun('ice-skate');
+
+function mondayWork(mondayTask = 'go to the office') {
+    return `This Monday, I will ${mondayTask}.`;
+}
+
+mondayWork('work from home');
+
+function wrapAdjective(String = "*") {
+    return function(whatIs = "special") {
+        return `You are ${whatIs}`;
+    }
+}
+
+
+// const encouragingPromptFunction = wrapAdjective("!!!");
